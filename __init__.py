7081de199bcf5369a3c19e8fc7a10fb164494c48
omrani-wiem <omraniwiem62@gmail.com>
2025-06-21 16:55:58 +0200

    correction et ajout de auth.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
 import os
-
 from flask import Flask
+from . import db  # importe db au bon endroit
 
 def create_app(test_config=None):
-    #creat and configure the app
-    app = Flask(__name__,instance_relative_config=True)
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-)
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
     if test_config is None:
-        app.config.from_pyfile('config.py' , silent=True)
+        app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
@@ -20,8 +20,10 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    db.init_app(app)  # <-- tu dois l’ajouter ici, pas après
+
     return app
