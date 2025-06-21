ee127908a8a14aea1fc945e5dfcb2253ad9d09d8
omrani-wiem <omraniwiem62@gmail.com>
2025-06-21 23:22:04 +0200

    ajout de deconnexion et exigence de l'authentification

--- auth.py
+++ auth.py
@@ -1,3 +1,15 @@
+#creation de blueprint
+import functools
+
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for
+                   )
+from werkzeug.security import check_password_hash, generate_password_hash #pour verifier et chiffrer les mots de pass et pour 
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+#crée un Blueprint, une sorte de module séparé pour tout ce qui est lié à l’authentification. Toutes ses routes commenceront par /auth.
+
 #register
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -54,3 +66,33 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+#avec partie connexion
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+#deconnexion
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+#Exiger l’authentification dans d’autres vues
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
