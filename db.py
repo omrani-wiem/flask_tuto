80920de0a678bb4ffbca2c337261d0b05f95f792
omrani-wiem <omraniwiem62@gmail.com>
2025-06-21 15:56:00 +0200

    partie 2

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ import click
 from flask import current_app,g
 from flask.cli import with_appcontext
 
-def get_db():
-    if 'db' not in g:
+def get_db():#get_db() : fonction qui retourne une connexion à la base.
+    if 'db' not in g: #'db' not in g : si la connexion n’a pas encore été enregistrée dans g.
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
@@ -20,3 +20,19 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+#partie 2
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
